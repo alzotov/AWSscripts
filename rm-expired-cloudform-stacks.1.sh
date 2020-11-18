4e77f093727e09debce6d9ba7eb76d35af1c6511
alzotov <azotov@hotmail.com>
2020-11-18 11:17:07 +0800

    improvements

--- rm-expired-cloudform-stacks.1.sh
+++ rm-expired-cloudform-stacks.1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #set -xe
-lmask=$1
+mask=$1
 let period=$2*86400
 echo $1 $mask $2 $period
 
@@ -8,7 +8,7 @@ function listStacks
 {
 	echo func $mask $period
 	aws --profile mfa cloudformation list-stacks --stack-status-filter CREATE_COMPLETE |  \
-	jq '.StackSummaries[] | {StackName,CreationTime,CreationTimeAWS: .CreationTime,CreationTimeISO: .CreationTime} |.CreationTimeISO |= (.[:-13]) |.CreationTime |= (.[:-13]|strptime("%Y-%m-%dT%H:%M:%S")|mktime) | select(.StackName|test("'$1'"))' | \
+	jq '.StackSummaries[] | {StackName,CreationTime,CreationTimeAWS: .CreationTime,CreationTimeISO: .CreationTime} |.CreationTimeISO |= (.[:-13]) |.CreationTime |= (.[:-13]|strptime("%Y-%m-%dT%H:%M:%S")|mktime) | select(.StackName|test("'$mask'"))' | \
 	jq 'if .CreationTime < (now - '$period') then . else empty end | .CreationTimeISO + " " + .StackName' | \
 	sed 's/"//g'
 }
@@ -21,7 +21,7 @@ fi
 echo run
 for s in \
     $(aws --profile mfa cloudformation list-stacks --stack-status-filter CREATE_COMPLETE |  \
-    jq '.StackSummaries[] | {StackName,CreationTime,CreationTimeISO: .CreationTime} |.CreationTime |= (.[:-13]|strptime("%Y-%m-%dT%H:%M:%S")|mktime) | select(.StackName|test("'$1'"))' | \
+    jq '.StackSummaries[] | {StackName,CreationTime,CreationTimeISO: .CreationTime} |.CreationTime |= (.[:-13]|strptime("%Y-%m-%dT%H:%M:%S")|mktime) | select(.StackName|test("'$mask'"))' | \
 	#tee >(cat) | \
 	jq 'if .CreationTime < (now - '$period') then . else empty end | .StackName' | \
     sed 's/"//g');
@@ -29,7 +29,7 @@ do
 	echo $s
     if [[ $3 != "-plan" ]]; then 
 		echo deleting stack: $s
-		#aws --profile mfa cloudformation delete-stack --stack-name $s; > /dev/null
+		aws --profile mfa cloudformation delete-stack --stack-name $s; 2> /dev/null
 	fi
 done
 
